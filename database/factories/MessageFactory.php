c67ad0f47df12613193afcd884b76a0c1a48aac8
delasthanos <delasthanos@gmail.com>
2024-06-30 21:28:38 +0300

    Add factories

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\MessageStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array{
+    return [
+      'message' => $this->faker->sentence,
+      'phone_number' => $this->faker->e164PhoneNumber(),
+      'user_id' => User::query()->first(),
+      'sms_provider_id' => User::query()->first()->defaultSmsProvider,
+      'message_status_id' => MessageStatus::where('status', 'delivered')->first(),
+    ];
+  }
+}
